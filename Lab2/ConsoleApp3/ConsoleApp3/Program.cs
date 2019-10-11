b7b8e89b0be067d95435e4489c3be5711c60e923
asssssss <ssss>
2019-10-11 08:42:02 +0200

    Zmiana1

---
+++ Lab2/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę: ");
+            string odpowiedz = Console.ReadLine();
+
+            int liczba = Convert.ToInt32(odpowiedz);
+
+            if (liczba % 2 == 0)
+            {
+                Console.WriteLine("Parzysta");
+            }
+            else
+            {
+                Console.WriteLine("Nieparzysta");
+            }
+        }
+    }
+}
