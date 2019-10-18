0659ad2b5b5e9f883c6b64068b4669e4af395d5b
PawelR <pawlo97@gmail.com>
2019-10-18 08:41:09 +0200

    Zmiana1

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = string.Empty;
+            while(tekst != "0")
+            {
+                tekst = Console.ReadLine();
+                int ocena;
+                bool czyPrzekonwertowano = int.TryParse(tekst, out ocena);
+            }
+        }
+    }
+}
