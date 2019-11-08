ff3f9ddd447aac87fe5fd87ae460581e3d99f7bf
PawelR <pawlo97@gmail.com>
2019-11-08 10:35:03 +0100

    Zmiana8

---
+++ Lab5/Lab5/Ksiazka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5
+{
+    class Ksiazka
+    {
+        public string Tytul;
+        public string Autor;
+        public int Regal;
+        public int Polka;
+        public int Miejsce;
+
+        public Ksiazka(string tytul, string autor, int regal, int polka, int miejsce)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            Regal = regal;
+            Polka = polka;
+            Miejsce = miejsce;
+        }
+
+    }
+}
--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -4,9 +4,58 @@ namespace Lab5
 {
     class Program
     {
+        /* Napisz program wspomagający magazyn biblioteki. Każda ksiązka ma tytuł. Magazyn ma 3 regały. Regał ma 6 półek. Półka ma 10 miejsc. Wskaż
+         gdzie szukać ksiązki na podstawie tytułu */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Ksiazka[,,] tab = new Ksiazka[3,6,10];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        tab[i, j, k] = new Ksiazka("Pan Tadeusz", "Mickiewicz",i,j,k);
+                    }
+                }
+            }
+            tab[2, 5, 3] = new Ksiazka("Potop", "Sienkiewicz",2,5,3); //Wykorzystanie klasy Ksiazka
+       
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        Console.WriteLine("Regał nr {0}, Półka nr {1}, Miejsce nr {2}", i, j, k);
+                        Console.WriteLine(tab[i, j, k].Tytul + " " + tab[i, j, k].Autor);
+                    }
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.Write("Podaj tytul lub autora ktorego szukasz: ");
+            string nazwa = Convert.ToString(Console.ReadLine());
+
+             for (int i = 0; i < 3; i++)
+             {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        if (nazwa == tab[i, j, k].Tytul || nazwa == tab[i, j, k].Autor)
+                        {
+                            Console.WriteLine("Ksiazka znajduje się w regale {0}, półce {1}, miejscu {2}", i, j, k);
+                        }                    
+                    }
+                 }
+             }
+  
         }
     }
 }
