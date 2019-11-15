19e5d5a9d2470f25027fcb35cc7faaf218fe731d
PawelR <pawlo97@gmail.com>
2019-11-15 10:41:47 +0100

    Zmian13

--- Lab6/Lab6/Samochod.cs
+++ Lab6/Lab6/Samochod.cs
@@ -6,6 +6,89 @@ namespace Lab6
 {
     public class Samochod
     {
-        public string Model;
+        private static int IloscModeli = 0;
+
+        public readonly string Model;      
+        
+        public int VIN { get; private set; }
+        public int Przebieg { get; private set; }
+        public void ZwiekszPrzebieg(int dystans)
+        {
+        if (dystans < 0)
+        {
+            throw new ArgumentException("Dystans nie moze byc ujemny");
+        }
+        }
+
+        private Samochod()
+        {
+            VIN = IloscModeli++;
+        }
+      
+        private int _przebieg;
+        /*
+        public int Przebieg
+        {
+            get { return _przebieg; }
+            set
+            {
+                if (value > Przebieg)
+                {
+                    _przebieg = value;
+                }
+            }
+        }
+        */
+
+        public int IloscDrzwi { get; private set; }
+
+        private int _waga;
+
+
+        public int Waga
+        {
+            get
+            {
+                return _waga;
+            }
+            set
+            {             
+                _waga = value;
+                if(_waga < 0) // Można także rzucić wyjątek
+                {
+                    _waga = 0;
+                }
+            }
+        }
+        void SetWaga(int value)
+        {
+            _waga = value;
+        }
+  
+            public Samochod(string model, int przebieg, int iloscDrzwi, int waga)
+            {
+                Model = model;
+                Przebieg = przebieg;
+                IloscDrzwi = iloscDrzwi;
+                Waga = waga;
+                VIN = IloscModeli++;
+            }
+
+        public Samochod(int id)
+        {
+            //var dane = Baza.GetById(id);
+            //Przebieg = dane.Przebieg;
+        }
+
+        public Samochod(int iloscDrzwi,int waga)
+            :this("Multipla",0, iloscDrzwi,waga)
+        {/* Tego nie musi już być dzięki this powyzej
+            Model = "Multipla";
+            Przebieg = 0;
+            IloscDrzwi = iloscDrzwi;
+            Waga = waga;
+           */
+        }
+
+        }
     }
-}
---
+++ Lab6/Lab6/stronaInternetowa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6
+{
+
+    class stronaInternetowa
+    {   //Dowolne rozszerzanie klasy, hermetyzacja wszystkiego co mozliwe
+        //Strona internetowa
+        //  Serwer
+        //      Szybkosc Procesora
+        //      Łącze
+        //      RAM
+        //      Technologia
+        //  Domena
+        //      Nazwa
+        //      Waznosc
+
+
+    }
+
+    class Serwer
+    {
+        public int SzybProcesora { get; private set; }
+        public int Lacze { get; private set; }
+        public int Ram { get; private set; }
+        public string Technologia { get; private set; }
+
+        public Serwer(int szybproc, int lacze, int ram, string technologia)
+        {
+            SzybProcesora = szybproc;
+            Lacze = lacze;
+            Ram = ram;
+            Technologia = technologia;
+        }
+    }
+
+    class Domena
+    {
+
+        public int Nazwa { get; private set; }
+        public DateTime Waznosc { get; private set; }
+
+        public Domena(int nazwa, DateTime waznosc)
+        {
+            Nazwa = nazwa;
+            Waznosc = waznosc;
+        }
+
+
+
+    }
+}
