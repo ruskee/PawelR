7ade19fcae16ecd828152f5fc650456a8f8ef295
PawelR <pawlo97@gmail.com>
2019-11-29 10:17:20 +0100

    Zmiana18

--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -1,14 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using RandomDataGenerator.Randomizers; //Biblioteki RandomDataGenerator
+using RandomDataGenerator.FieldOptions; 
 
 namespace Lab7
 {
+    public class Osoba
+    {
+        public int id;
+        public string imie;
+        public string nazwisko;
+
+        public Osoba(int id, string imie, string nazwisko)
+        {
+            this.id = id;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-           
+            /*
+            List<int> lista = Enumerable.Range(100, 150).ToList();
+            List<int> podzielnePrzez3 = lista.Where(x => x % 3 == 0).ToList();
+            double srednia = podzielnePrzez3.Average();
+            int suma = podzielnePrzez3.Sum();
+
+            int nrStrony = 1;
+            int elemNaStronie = 10;
+
+            
+            foreach (var item in podzielnePrzez3)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nSrednia to {0}", srednia);
+            Console.WriteLine("\nSuma to {0}", suma);
+            
+
+            //IEnumerable wystarcza do stworzenie fora
+            IEnumerable<int> strona = lista.Skip(elemNaStronie * (nrStrony - 1)).Take(elemNaStronie);
+
+            foreach(var item in strona)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+            List<Osoba> osoby = lista.Select(
+                x => new Osoba()
+                {
+                    id = x,
+                    imie = x.ToString(),
+                    nazwisko = "aaa"
+                }
+                ).ToList();
+
+            osoby[45].nazwisko = "bbb";
+            
+            List<string> nazwiska = osoby.Select(x => x.nazwisko).ToList();
+            List<string> unikalne_nazwiska = osoby.Select(x => x.nazwisko).Distinct().ToList();
+            
+            Osoba szukana = osoby.FirstOrDefault(x => x.nazwisko == "ccc");
+            Console.WriteLine($"{szukana.id}:{szukana.imie} {szukana.nazwisko}");
+            */
+            var nameGen = RandomizerFactory.GetRandomizer(new FieldOptionsFirstName());
+            var lastnameGen = RandomizerFactory.GetRandomizer(new FieldOptionsLastName());
+
+            List<Osoba> osoby = Enumerable.Range(100, 150)
+                .Select(x=>
+                new Osoba(
+                    x,
+                    nameGen.Generate(),
+                    lastnameGen.Generate())
+                    ).ToList();
+
+            List<Osoba> alfabetycznie = osoby.OrderBy(x=>x.nazwisko).ThenBy(x=>x.imie).ToList();
+            
+            foreach (var item in alfabetycznie)
+            {
+                
+                Console.WriteLine($"{item.id}: {item.imie} {item.nazwisko}");
+            
+            }
+         
+
         }
     }
 }
