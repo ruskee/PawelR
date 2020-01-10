867a2e1097e556797fc57c860c0d9a3eb1715667
ruskee <pawlo97@gmail.com>
2020-01-10 10:03:55 +0100

    Change11

--- Lab10/Lab10/Klasa.cs
+++ Lab10/Lab10/Klasa.cs
@@ -4,14 +4,16 @@ using System.Text;
 
 namespace Lab10
 {
+    [Flags]
     enum TypOsoby
     {
-        Dziecko,
-        Mlodzierz,
-        Dorosly,
-        Starszy
+        Dziecko = 1, // [0001]
+        Mlodzierz = 2, // [0010]
+        Dorosly = 4, // [0100]
+        Starszy = 8 // [1000]
     }
 
+    [Flags]
     enum Zainteresowania
     {
         Sport,
