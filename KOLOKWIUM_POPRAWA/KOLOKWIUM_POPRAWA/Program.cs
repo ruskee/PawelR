9e18c6390c7689e8fc9ec2785073103d304be0eb
ruskee <pawlo97@gmail.com>
2020-01-17 14:31:32 +0100

    Kolokw

---
+++ KOLOKWIUM_POPRAWA/KOLOKWIUM_POPRAWA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KOLOKWIUM_POPRAWA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
