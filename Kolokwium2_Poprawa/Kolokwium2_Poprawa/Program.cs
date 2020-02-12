8810e8ec688062df7d54be52673c67d7a15b1063
ruskee <pawlo97@gmail.com>
2020-02-12 17:12:51 +0100

    Kolokwium2

---
+++ Kolokwium2_Poprawa/Kolokwium2_Poprawa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kolokwium2_Poprawa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
